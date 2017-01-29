6fb4a978fd7ceefe63705782daa4e765abde1e4f
Tony S Yu <tsyu80@gmail.com>
2017-01-29 10:49:58 -0600

    Add django_admin_bootstrapped

--- djangosurvey/settings.py
+++ djangosurvey/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

ac43319ca34d74b992d480cda6cdc4515a9c3715
Tony S Yu <tsyu80@gmail.com>
2017-01-29 13:32:54 -0600

    Add footer to display user

--- djangosurvey/urls.py
+++ djangosurvey/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
-    url(r'^', include('polls.urls')),
+    url(r'^', include('polls.urls', namespace='polls')),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
